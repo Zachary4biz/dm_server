a6182c11ea306e5b1769700838ff65b97a124606
zhoutong <zhoutong@apusapps.com>
2019-07-09 20:04:57 +0800

    fix-bug: timeit

--- CVServer/util/common_util.py
+++ CVServer/util/common_util.py
@@ -5,16 +5,25 @@
 import time
 import functools
 
+
 # def timeit(func):
 #     a = time.time()
 #     b = time.time()
 
 
-def timeit(func):
+def deco_timeit(func):
     @functools.wraps(func)
     def wrapper(*args, **kw):
         b = time.time()
         result = func(*args, **kw)
         e = time.time()
-        return result, round(e-b, 4)
+        return result, round(e - b, 4)
+
     return wrapper
+
+
+def timeit(func):
+    b = time.time()
+    result = func
+    e = time.time()
+    return result, round(e - b, 4)
