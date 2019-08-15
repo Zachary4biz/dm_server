12326da84c66b9b8f99e2c0b32f751dbfdca78f2
zhoutong <zhoutong@apusapps.com>
2019-08-15 17:12:27 +0800

    update: BJ time & err_logfile

--- CVServer/util/logger.py
+++ CVServer/util/logger.py
@@ -9,13 +9,16 @@
 
 import logging.handlers
 import sys
+import time
 import datetime
 import os
 from os.path import dirname
-
+from pytz import timezone
 
 DEFAULT_LOGGING_LEVEL = logging.INFO
-#DEFAULT_LOGGING_LEVEL = logging.DEBUG
+
+
+# DEFAULT_LOGGING_LEVEL = logging.DEBUG
 
 
 class Logger(object):
@@ -25,7 +28,7 @@ class Logger(object):
     def __init__(self, loggername,
                  loglevel2console=DEFAULT_LOGGING_LEVEL,
                  loglevel2file=DEFAULT_LOGGING_LEVEL,
-                 log2console=True, log2file=False, logfile=None,formatstr=None):
+                 log2console=True, log2file=False, logfile=None, logfile_err=None, formatstr=None):
         """Logger initialization
         Args:
             loggername: Logger name, the same name gets the same logger instance
@@ -43,7 +46,7 @@ class Logger(object):
         self.logger.setLevel(logging.DEBUG)
 
         # set formater
-        logging.Formatter.converter = self.beijing_time
+        logging.Formatter.converter = self.beijing
         formatstr = '|%(asctime)s| [%(levelname)s] [%(filename)s-%(lineno)d] %(message)s' if formatstr is None else formatstr
         formatter = logging.Formatter(formatstr, "%Y-%m-%d %H:%M:%S")
 
@@ -58,15 +61,39 @@ class Logger(object):
             # Create a handler for writing to the log file
             # fh = logging.FileHandler(logfile)
             # Create a handler for changing the log file once a day, up to 15, scroll delete
-            fh = logging.handlers.TimedRotatingFileHandler(logfile, when='D', interval=1, backupCount=15, encoding='utf-8')
-            fh.setLevel(loglevel2file)
-            fh.setFormatter(formatter)
-            self.logger.addHandler(fh)
+            fh_defualt = logging.handlers.TimedRotatingFileHandler(logfile, when='D', interval=1, backupCount=15,
+                                                                   encoding='utf-8')
+            fh_defualt.setLevel(loglevel2file)
+            fh_defualt.setFormatter(formatter)
+            if logfile_err is not None:
+                # 1. 传err文件时，普通的handler只处理INFO级别
+                info_filter = logging.Filter()
+                info_filter.filter = lambda record: record.levelno < logging.WARNING  # 设置过滤等级
+                fh_defualt.addFilter(info_filter)
+                # 2. 新增一个专门处理 WARN和ERROR的handler
+                err_filter = logging.Filter()
+                err_filter.filter = lambda record: record.levelno >= logging.WARNING
+                fh_err = logging.handlers.TimedRotatingFileHandler(logfile_err, when='D', interval=1, backupCount=15,
+                                                                   encoding='utf-8')
+                fh_err.setLevel(loglevel2file)
+                fh_err.setFormatter(formatter)
+                fh_err.addFilter(err_filter)
+                self.logger.addHandler(fh_err)
+            self.logger.addHandler(fh_defualt)
 
     def get_logger(self):
         return self.logger
 
     @staticmethod
-    def beijing_time(*args, **kwargs):
-        beijing_time = datetime.datetime.now() + datetime.timedelta(hours=8)
+    def beijing(sec, what):
+        beijing_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
         return beijing_time.timetuple()
+
+
+if __name__ == '__main__':
+    # logger = Logger('cutcut_profile', log2console=False, log2file=True, logfile="abcd.log").get_logger()
+    logger = Logger('cutcut_profile', log2console=False, log2file=True, logfile="abcd.log",
+                    logfile_err="abcd_err.log").get_logger()
+    logger.debug("a debug")
+    logger.info("an info")
+    logger.error("an error")
