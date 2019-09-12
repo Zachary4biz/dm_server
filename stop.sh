da50562f0b65d5a1cf470373376f3a1a3ec09c79
zhoutong <zhoutong@apusapps.com>
2019-09-12 16:10:33 +0800

    format: 增加log

--- stop.sh
+++ stop.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 pid_list=`ps -ef | grep "manage_cutcut_server.py runserver" | grep -v "grep" | awk '{print $2}'`
 for res in ${pid_list}; do kill -9 ${res}; done
+echo "remaining as follow:"
 ps -ef | grep "cutcut"
 
 
