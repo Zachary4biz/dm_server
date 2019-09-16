e97236aed1c7b491bfe43502875f7430eaed763d
zhoutong <zhoutong@apusapps.com>
2019-09-16 12:45:14 +0800

    fixbug: add params

--- start.sh
+++ start.sh
@@ -17,6 +17,6 @@ else
 fi
 
 localIP=`ifconfig | grep -Eo 'inet [0-9\.]+' | grep -v 127.0.0.1 | grep -Eo '[0-9\.]+' | head -1` # 类似实体机如mac有多个网口（wifi和usb网线）取第一个
-python -u _start_service_separate.py --service service_name --host localIP
+python -u _start_service_separate.py --service ${service_name} --host ${localIP}
 
 
