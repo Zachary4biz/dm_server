9ae6a4d09dab61ec3398ca36a5f87448fdd5d1a0
zhoutong <zhoutong@apusapps.com>
2019-09-23 14:22:33 +0800

    update: 替换参数"profile"为"cutcut_profile"

--- stop.sh
+++ stop.sh
@@ -7,7 +7,7 @@ service_name=$1
 if [[ ! -n "${service_name}" ]]; then
     echo ">>> 未指定服务"
     echo " all      关闭 [所有] 服务"
-    echo " profile  关闭 [画像总成] 服务"
+    echo " cutcut_profile  关闭 [画像总成] 服务"
     echo " age      关闭 [年龄检测] 服务"
     echo " gender   关闭 [性别检测] 服务"
     echo " nsfw     关闭 [鉴黄] 服务"
