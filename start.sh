68b781e03f475c11426a6633bccf6b59f011131e
zhoutong <zhoutong@apusapps.com>
2019-10-08 18:46:41 +0800

    format: format cmd

--- start.sh
+++ start.sh
@@ -5,12 +5,13 @@ conda activate cv3.6
 service_name=$1
 if [[ ! -n "${service_name}" ]]; then
     echo ">>> 未指定服务:"
-    echo " all      开启 [所有] 服务"
-    echo " cutcut_profile  开启 [画像总成] 服务"
-    echo " age      开启 [年龄检测] 服务"
-    echo " gender   开启 [性别检测] 服务"
-    echo " nsfw     开启 [鉴黄] 服务"
-    echo " obj      开启 [目标检测] 服务"
+    echo " 参数             port(default)  usage"
+    echo " all             |*   |         开启 [所有] 服务"
+    echo " age             |8001|         开启 [年龄检测] 服务"
+    echo " gender          |8002|         开启 [性别检测] 服务"
+    echo " nsfw            |8003|         开启 [鉴黄] 服务"
+    echo " obj             |8004|         开启 [目标检测] 服务"
+    echo " cutcut_profile  |8000|         开启 [画像总成] 服务"
     exit 1
 else
     echo "将启动服务: ${service_name}"
