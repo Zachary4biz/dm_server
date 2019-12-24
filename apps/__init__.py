57cf88b0a98cd744f70dec197d3804ebb42d367a
zhoutong <zhoutong@apusapps.com>
2019-12-24 16:52:28 +0800

    update: add support for import module
    这个是目前为了让config里获取到apps里的module目录，进而获取相应的model模型用来启动TFServing

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,3 +1,12 @@
 # author: zac
 # create-time: 2019-07-09 14:19
-# usage: - 
+# usage: -
+# import一些希望外部能直接以 from apps import age 这种形式使用的包
+
+import age
+import ethnicity
+import gender
+import nsfw
+import obj_detection
+import vectorize
+import cutcut
