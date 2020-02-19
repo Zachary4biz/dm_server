98709f8cabbb30ce22f19b966760f5a520cad911
work <work@vm-data-rec-imgprofile001.eq-sg-2.apus.com>
2020-02-19 18:30:27 +0800

    update: sort by keys to use correct result of yolo
    .pb 返回的结果有时候会有不同的key比如之前是yolo_nms_1新的模型里发现变成了yolo_nms_1_1
    
    增加了注释说明obj的分数映射在ensemble中完成

--- apps/nsfw/nsfw_obj/nsfw_obj_service.py
+++ apps/nsfw/nsfw_obj/nsfw_obj_service.py
@@ -38,13 +38,17 @@ def _predict(imgPIL):
         # 注：这里modelClassifier已经取了tfserving结果的第0项，同时这也意味着只支持每次预测一个图片
         pred = modelClassifier.predict(imgArr)
         # obj的返回结果有些特殊 逐个取
-        boxes=pred['yolo_nms']
-        probs=pred['yolo_nms_1']
-        classes=pred['yolo_nms_2']
-        num=pred['yolo_nms_3']
+        # boxes=pred['yolo_nms']
+        # probs=pred['yolo_nms_1']
+        # classes=pred['yolo_nms_2']
+        # num=pred['yolo_nms_3']
+        kv_sorted=sorted([(k,v) for k,v in pred.items()],key=lambda x:x[0])
+        boxes,probs,classes,num=[v for k,v in kv_sorted]
         # 检测到的所有物体
         label_prob_list=[(int(classes[i]),probs[i]) for i in range(num)]
-        label_prob_list=[lp for lp in label_prob_list if lp[1]>=obj_prob_hold]
+        # 物体检测针对的是裸露器官，加上0.3在目标检测中就算是可靠阈值
+        # 所以实际作为鉴黄分数和bcnn一起使用的时候要加倍或者其他放大方式 | 这个在nsfw_ensemble里实现
+        label_prob_list=[(label,prob) for label,prob in label_prob_list if prob>=obj_prob_hold]
         res = [{"label":l,"prob":p,"name":output[l]} for l,p in label_prob_list]
         return res, "success"
     except TFServingModel.CustomException as e:
